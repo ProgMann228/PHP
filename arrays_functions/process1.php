ed17ee789536fc67c0f3785797a771faf3c07f9d
Mona Lisa <105809375+ProgMann228@users.noreply.github.com>
2025-09-29 13:47:29 +0300

    Add files via upload

---
+++ arrays_functions/process1.php
@@ -0,0 +1,21 @@
+<?php
+
+function capMe(array $arr):array{
+    foreach ($arr as &$el){
+        //echo $el;
+        $first=strtoupper($el[0]);
+        $last=strtolower($el[strlen($el)-1]);
+        if(strlen($el)>2) $mid=strtolower(substr($el,1,strlen($el)-2));
+        else if(strlen($el)===1) {
+            $mid='';
+            $last='';
+        }
+        else $mid='';
+
+        $el=$first.$mid.$last;
+    }
+    unset($el);
+    return $arr;
+
+}
+?>
