51619d45101c82de8e25782f7086010eca82b37a
Mona Lisa <105809375+ProgMann228@users.noreply.github.com>
2025-10-01 15:25:02 +0300

    Add files via upload

---
+++ OOP/task_6/task_6.php
@@ -0,0 +1,45 @@
+<?php
+
+class Person
+{
+    const species = "Homo Sapiens";
+    public $name;
+    public $age;
+    public $occupation;
+
+    public function __construct($name, $age, $occupation)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->occupation = $occupation;
+    }
+
+    public function introduce()
+    {
+        return "Hello, my name is $this->name";
+    }
+
+    final public function describe_job()
+    {
+        return "I am currently working as a(n) $this->occupation";
+    }
+
+    final public function greet_extraterrestrials($species)
+    {
+        return "Welcome to Planet Earth $species!";
+    }
+
+}
+
+class ComputerProgrammer extends Person{
+    public function __construct($name, $age,$occupation){
+        parent::__construct($name, $age,$occupation);
+    }
+    public function introduce()
+    {
+        return "Hello, my name is $this->name and I am a $this->occupation";
+    }
+}
+
+
+?>
